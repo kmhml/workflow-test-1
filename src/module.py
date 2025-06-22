20d9c851cd78c347e0fc893a68b8cd70e22ce59d
kmhml <kumagai.taizou@gmail.com>
2025-06-22 12:09:06 +0900

    最初のコミット

---
+++ src/module.py
@@ -0,0 +1,9 @@
+def foo(a, b):
+    """
+    This function takes two arguments and returns their sum.
+    
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
