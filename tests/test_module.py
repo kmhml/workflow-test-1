3d2d51cbf3f7649f3fd5d0915e7be5274e8bd7d9
kmhml <kumagai.taizou@gmail.com>
2025-06-22 13:03:57 +0900

    failureを確認

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,5 +2,5 @@ from src.module import foo
 
 def test_foo():
     assert foo(1, 2) == 3
-    assert foo(0, 0) == 0
-    # assert foo(0, 0) == 1
+    # assert foo(0, 0) == 0
+    assert foo(0, 0) == 1
